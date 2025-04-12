31b98db43aa84424294fb1a0ed7386ee6d1375cc
sossou7 <mailto:hermafa7@gmail.com>
2025-04-12 17:29:49 +0200

    connexion  à la base de donnée

---
+++ db.php
@@ -0,0 +1,17 @@
+<?php
+// CONNEXION A LA BASE DE DONNEES
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "ecoride";
+
+
+try{  
+$conn =new PDO("mysql:host=$servername; dbname=$dbname", $username, $password);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+
+catch(PDOException $e){
+echo"Erreur de connexion". $e->getMessage();
+}
+?>
